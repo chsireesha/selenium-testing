2dd095fdb3353db70c53e7f622d6102b7b6e2adf
Sireesha Chintaguntla <Sireesha Chintaguntla>
2017-07-15 19:28:38 +0530

    Selenium code to actitime login and logout

---
+++ src/test/java/com/selenium/ActitimeLoginAndLogout.java
@@ -0,0 +1,33 @@
+package com.selenium;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ActitimeLoginAndLogout {
+public static WebDriver driver;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		driver=new FirefoxDriver();
+		driver.get("http://localhost/login.do");
+	}
+	@Test
+	public void testActitimeLoginAndLogout() {
+		driver.findElement(By.id("username")).sendKeys("admin");
+		driver.findElement(By.name("pwd")).sendKeys("manager");
+		driver.findElement(By.id("keepLoggedInCheckBox")).click();
+		driver.findElement(By.id("loginButton")).click();
+		driver.findElement(By.id("logoutLink")).click();
+		String title = driver.getTitle();
+		assertEquals("actiTIME - Login", title);
+	}
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		driver.close();
+	}
+}
