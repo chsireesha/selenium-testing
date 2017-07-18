05453ed705e83b9cf661b91ba80504b6d7b21266
Sireesha Chintaguntla <Sireesha Chintaguntla>
2017-07-18 15:05:56 +0530

    Adding tests for copy the value from one textbox to another text box if data is enabled

---
+++ src/test/java/com/selenium/EnableTextTransfer.java
@@ -0,0 +1,37 @@
+package com.selenium;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class EnableTextTransfer {
+	public static WebDriver driver;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		driver=new FirefoxDriver();
+		driver.get(new File("./html/enable.html").toURI().toURL().toString());
+	}
+
+	@Test
+	public void testEnableTextTransfer() {
+		String un = driver.findElement(By.id("un")).getAttribute("value");
+		WebElement fn = driver.findElement(By.id("fn"));
+		fn.sendKeys(un);
+		String attribute = fn.getAttribute("value");
+		assertEquals("I Love Selenium", attribute);
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		driver.close();
+	}
+}
