aa0a5cd9ca855f5deb70639b5ed3b3bdacbae066
Sireesha Chintaguntla <Sireesha Chintaguntla>
2017-07-18 10:43:20 +0530

    MultiSelect ListBox using Xpath by text

---
+++ src/test/java/com/selenium/MultiSelectListBoxVerification.java
@@ -0,0 +1,45 @@
+package com.selenium;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MultiSelectListBoxVerification {
+	public static WebDriver driver;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		driver=new FirefoxDriver();
+		driver.get(new File("./html/mslb.html").toURI().toURL().toString());
+
+	}
+
+	@Test
+	public void testMultiSelectListBoxVerification() {
+		WebElement element = driver.findElement(By.xpath("//option[text()='vada']/following-sibling::*[1]"));
+		String actual = element.getText();
+		assertEquals("poori", actual);
+		WebElement element1 = driver.findElement(By.xpath("//option[text()='vada']/following-sibling::*[last()]"));
+		String actual1 = element1.getText();
+		assertEquals("chapati", actual1);
+		WebElement element2 = driver.findElement(By.xpath("//option[text()='vada']/preceding-sibling::*[1]"));
+		String actual2 = element2.getText();
+		assertEquals("dosa", actual2);
+		WebElement element3 = driver.findElement(By.xpath("//option[text()='vada']/preceding-sibling::*[last()]"));
+		String actual3 = element3.getText();
+		assertEquals("idly", actual3);
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		driver.close();
+	}
+}
--- src/test/java/com/selenium/XpathVerificationTest.java
+++ src/test/java/com/selenium/XpathVerificationTest.java
@@ -32,4 +32,5 @@ public class XpathVerificationTest {
 	public static void tearDownAfterClass() throws Exception {
 		driver.close();
 	}
+	
 }
