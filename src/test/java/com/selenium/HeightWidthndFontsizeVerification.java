3e4bafb083cf1475b5d89722c032337995a3b587
Sireesha Chintaguntla <Sireesha Chintaguntla>
2017-07-18 20:37:36 +0530

    Adding tests to find the height,width and font-size of a textbox

---
+++ src/test/java/com/selenium/HeightWidthndFontsizeVerification.java
@@ -0,0 +1,41 @@
+package com.selenium;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class HeightWidthndFontsizeVerification {
+	public static WebDriver driver;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		driver=new FirefoxDriver();
+		driver.get(new File("./html/login.html").toURI().toURL().toString());
+	}
+
+	@Test
+	public void testHeightWidthndFontSize() {
+		WebElement un = driver.findElement(By.id("username"));
+		//to know the height and width of text field
+		Dimension size = un.getSize();
+		System.out.println(size);
+		//to know the font-size of text field
+		String cssValue = un.getCssValue("font-size");
+		System.out.println(cssValue);
+		assertTrue(cssValue.contains("px"));
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		driver.close();
+	}
+}
