7372c79506b50732893e008712f54b86b2267157
Sireesha Chintaguntla <Sireesha Chintaguntla>
2017-07-19 12:55:38 +0530

    Adding tests to page scroll down

---
+++ src/test/java/com/selenium/PageScrollDownVerification.java
@@ -0,0 +1,31 @@
+package com.selenium;
+
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class PageScrollDownVerification {
+	public static WebDriver driver;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		driver=new FirefoxDriver();
+		driver.get("http://www.seleniumhq.org/");
+	}
+
+	@Test
+	public void testPageScrollDownVerification() {
+		JavascriptExecutor js=(JavascriptExecutor)driver;
+		js.executeScript("window.scrollBy(0,250)");
+		driver.close();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		driver.close();
+	}
+}
