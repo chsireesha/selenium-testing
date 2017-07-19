21f52d8896e6c61fd7ac74dba48412de9d3ea124
Sireesha Chintaguntla <Sireesha Chintaguntla>
2017-07-19 14:52:04 +0530

    Adding tests to get the home page title and login page title

---
+++ src/test/java/com/selenium/SynchronizationVerification.java
@@ -0,0 +1,47 @@
+package com.selenium;
+
+import static org.junit.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SynchronizationVerification {
+	public static WebDriver driver;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		driver=new FirefoxDriver();
+		driver.get("https://demo.vtiger.com/");
+		//implicit wait
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void testSynchronizationVerification() {
+		//to get the home page title
+		String home = driver.getTitle();
+		assertEquals("vtiger", home);
+		driver.findElement(By.id("username"));
+		driver.findElement(By.id("password"));
+		driver.findElement(By.xpath("//button[.='Sign in']")).click();
+		//explicit wait
+		WebDriverWait wait=new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.titleContains("Home"));
+		//to get the login page title
+		String login = driver.getTitle();
+		assertEquals("Home", login);
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		driver.close();
+	}
+}
